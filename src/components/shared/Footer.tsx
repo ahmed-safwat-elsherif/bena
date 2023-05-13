eb6115f6b5bdcbade5e2d85732913a9fa0f2d1a6
Ahmed SAFWAT-ELSHERIF <ahmed.safwat-elsherif@valeo.com>
2023-05-13 13:31:08 +0300

    Add the responsiveness for footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -4,18 +4,18 @@ import { ReactComponent as AppFooterLogo } from "../../svg/app-logo-footer.svg";
 const Footer = () => {
   return (
     <footer className="container mt-24">
-      <div className="mb-16 flex justify-between border-t-2 border-babyblue-dark pt-14">
-        <div className="flex flex-1 items-start space-x-2 rtl:space-x-reverse">
+      <div className="mb-16 flex flex-col items-center justify-between space-y-10 border-t-2 border-babyblue-dark pt-14 md:flex-row md:items-start md:space-y-0">
+        <div className="flex max-w-md flex-1 flex-col items-center space-x-2 rtl:space-x-reverse md:flex-row md:items-start">
           <div className="flex-1">
-            <AppFooterLogo />
+            <AppFooterLogo className="w-[4rem] md:w-[6.875rem]" />
           </div>
-          <p className="text-sm font-medium leading-7 text-babyblue-dark">
+          <p className="text-center text-sm font-medium leading-7 text-babyblue-dark md:text-start">
             هي مبادرة أطلقتها وزارة التضامن الإجتماعي تحت رعاية الرئيس عبد
             الفتاح السيسي لرفع وتحسين جودة الخدمات التي تقدمها مؤسسات الرعاية
             الإجتماعية
           </p>
         </div>
-        <div className="flex flex-1 justify-evenly font-semibold">
+        <div className="flex flex-1 justify-evenly space-x-10 font-semibold rtl:space-x-reverse md:space-x-0">
           <ul className="flex flex-col items-start space-y-3">
             <li>عن المبادرة</li>
             <li>أحدث الأخبار</li>
@@ -28,7 +28,7 @@ const Footer = () => {
             <li>تابعنا</li>
           </ul>
         </div>
-        <div className="flex items-end rtl:space-x-reverse">
+        <div className="flex items-end justify-center rtl:space-x-reverse">
           <Ministry />
         </div>
       </div>
